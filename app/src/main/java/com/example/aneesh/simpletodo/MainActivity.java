1cad5594a3667541877ead15d4a2bd45d3ad14ac
aneeshkarayil <aneeshkarayil@gmail.com>
2017-01-24 19:36:12 +0530

    Completing deleting items

--- app/src/main/java/com/example/aneesh/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/aneesh/simpletodo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aneesh.simpletodo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.aneesh.simpletodo.Utils.TaskUtils;
 import com.example.aneesh.simpletodo.activity.EditItemActivity;
@@ -20,6 +22,7 @@ import com.example.aneesh.simpletodo.adapter.TasksAdapter;
 import com.example.aneesh.simpletodo.model.Task;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -140,8 +143,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void deleteCheckedItems() {
-        List<Task> checkedItems = new ArrayList<>();
-        List<Task> childItems = new ArrayList<>();
+        final List<Task> checkedItems = new ArrayList<>();
+        final List<Task> childItems = new ArrayList<>();
         for (Task task: tasks)
         {
             if (task.isDone())
@@ -167,6 +170,12 @@ public class MainActivity extends AppCompatActivity {
                     .content("Are you sure you want to delete "+checkedItems.size()+" checked items?")
                     .positiveText(R.string.agree)
                     .negativeText(R.string.disagree)
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            deleteFromList(checkedItems, childItems);
+                        }
+                    })
                     .show();
         }
         else
@@ -176,8 +185,32 @@ public class MainActivity extends AppCompatActivity {
                     .content("Are you sure you want to delete "+checkedItems.size()+" checked items and " + childItems.size()+" sub-items?")
                     .positiveText(R.string.agree)
                     .negativeText(R.string.disagree)
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            deleteFromList(checkedItems, childItems);
+                        }
+                    })
                     .show();
         }
+
+    }
+
+    private void deleteFromList(List<Task> checkedItems, List<Task> childItems) {
+
+        List<Task> newTaskList = TaskUtils.generateTasks();
+
+        Iterator<Task> iterator = newTaskList.iterator();
+        while (iterator.hasNext())
+        {
+            Task task = iterator.next();
+            if (checkedItems.contains(task) || childItems.contains(task))
+            {
+                iterator.remove();
+            }
+        }
+
+        swapAdapterData(newTaskList);
 
     }
 
