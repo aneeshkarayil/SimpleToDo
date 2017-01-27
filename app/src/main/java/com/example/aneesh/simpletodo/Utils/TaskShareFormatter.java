dfc65fef0f996992f4d332491391f6a5a9ee687e
aneeshkarayil <aneeshkarayil@gmail.com>
2017-01-27 23:52:08 +0530

    Initial stub for sharing

--- app/src/main/java/com/example/aneesh/simpletodo/Utils/TaskShareFormatter.java
+++ app/src/main/java/com/example/aneesh/simpletodo/Utils/TaskShareFormatter.java
@@ -17,6 +17,7 @@ public class TaskShareFormatter {
     public static String getFormattedTask(UUID parentTaskID)
     {
         List<Task> taskList = TaskUtils.generateTasks();
+        StringBuilder builder = new StringBuilder();
 
         Map<UUID,List<Task>> tree  = new HashMap<UUID,List<Task>>();
         List<Task>           roots = new ArrayList<Task>();
@@ -32,8 +33,34 @@ public class TaskShareFormatter {
 
         tree.put(null, roots);
 
-        return "TODO: String";
+        for (Task root: roots)
+        {
+            builder.append("\t"+getTaskHierarchy(root, tree)+"\n");
+        }
+
+        return builder.toString();
     }
 
+
+    public static String getTaskHierarchy(Task task, Map<UUID,List<Task>>  uuidMap)
+    {
+        String description = task.getDescription()+"\n";
+
+        if (uuidMap.get(task.getTaskId()) == null)
+        {
+            return description;
+        }
+
+        List<Task> tasks = uuidMap.get(task.getTaskId());
+
+        String childDescription = "";
+
+        for (Task childTasks: tasks)
+        {
+            childDescription = childDescription + "\t -"+childTasks.getDescription()+"\n"+ getTaskHierarchy(childTasks, uuidMap);
+        }
+
+        return description + childDescription;
+    }
 
 }
