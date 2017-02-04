a16a7f6a3de412968de3b3061eb3cdd0e16f26f5
aneeshkarayil <aneeshkarayil@gmail.com>
2017-02-04 15:55:57 +0530

    Implementing Up action for settings activity

--- app/src/main/java/com/example/aneesh/simpletodo/activity/SettingsActivity.java
+++ app/src/main/java/com/example/aneesh/simpletodo/activity/SettingsActivity.java
@@ -3,8 +3,10 @@ package com.example.aneesh.simpletodo.activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.aneesh.simpletodo.R;
 import com.example.aneesh.simpletodo.fragment.SettingsFragment;
@@ -21,9 +23,22 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
         toolbar.setTitle(R.string.settings);
-        toolbar.setLogo(R.drawable.ic_arrow_back);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
