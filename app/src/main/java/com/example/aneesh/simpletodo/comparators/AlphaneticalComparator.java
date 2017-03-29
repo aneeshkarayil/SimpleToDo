8648fa768d25bcefc0d8043b94746a7ca104c945
aneeshkarayil <aneeshkarayil@gmail.com>
2017-03-29 07:13:58 +0530

    Sorting items - basic comparators

--- app/src/main/java/com/example/aneesh/simpletodo/comparators/AlphaneticalComparator.java
+++ app/src/main/java/com/example/aneesh/simpletodo/comparators/AlphaneticalComparator.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
  */
 
 public class AlphaneticalComparator implements Comparator<Task> {
-
     @Override
     public int compare(Task task1, Task task2) {
         return task1.getDescription().compareTo(task2.getDescription());
